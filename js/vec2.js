367ae753f846eca8941c269c9177a09f9b088c66
P H <p.houska@cgv.tugraz.at>
2023-08-05 13:10:31 +0200

    added Vec2 - determinant (=cross2d)

--- js/vec2.js
+++ js/vec2.js
@@ -32,6 +32,23 @@ class Vec2 {
     static sub( v1, v2 ) {
         return new Vec2( v1.x-v2.x, v1.y-v2.y );
     }
+
+    static det( v1, v2 ) {
+        return v1.x * v2.y - v1.y * v2.x;
+    }
+    
+    det( v ) {
+        return Vec2.det( this, v );
+    }
+    
+    
+    static cross2( v1, v2 ) {
+        return Vec2.det( v1, v2 );
+    }
+    
+    cross2( v ) {
+        return Vec2.det( this, v );
+    }
     
     scale(s) {
         this.x *= s;
