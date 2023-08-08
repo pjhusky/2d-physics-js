d7cbe29fe4a19fc4b1607b0db8695f66d1a964da
P H <p.houska@cgv.tugraz.at>
2023-08-08 23:59:42 +0200

    Center-of-mass is also center of coordinates, (debug)bounding circle is still rendered correctly * TODO Container for primitive + debug bounding circle

--- js/rigidBody.js
+++ js/rigidBody.js
@@ -25,6 +25,7 @@ class RigidBody {
     }
     
     getCenterOfMass() { return this.center_of_mass; }
+    getBoundingCircle() { return [ this.center_of_mass, 0.0 ]; }
     
     toString() { return `'${this.constructor.name}'`; }
 }
@@ -39,6 +40,7 @@ class RigidBody_Circle extends RigidBody {
     }
     
     getBoundRadius() { return this.radius; }
+    getBoundingCircle() { return [ this.center_of_mass, this.radius ]; }
 }
 
 class RigidBody_Polygon extends RigidBody {
@@ -49,12 +51,13 @@ class RigidBody_Polygon extends RigidBody {
         this.shape_type = ShapeType.polygon;
         this.bound_radius = -1;
         this.relative_path_points_ccw = relative_path_points_ccw;
+        this.bounding_circle = new Array();
         this.getBoundRadius();
+        this.getBoundingCircle();
     }
     
-    getBoundRadius() {
-        if ( this.bound_radius < 0 ) {
-            // TODO: calculate!!!
+    getBoundingCircle() {
+        if ( this.bound_radius < 0.0 ) {
             //console.error( `TODO: calculate bound radius ${this}` );
             
             // const com = this.getCenterOfMass();
@@ -76,48 +79,46 @@ class RigidBody_Polygon extends RigidBody {
             const com_v = new Vec2( sum_x, sum_y );
             console.log( `com_v = ${com_v}` );
 
-            // recenter pivot to object-space origin
+            // recenter pivot (center of mass) to object-space origin
             for ( let i = 0; i < this.relative_path_points_ccw.length; i++ ) {
                 this.relative_path_points_ccw[i][0] -= com_v.x;
                 this.relative_path_points_ccw[i][1] -= com_v.y;
             }           
 
             // TEST
-            {
-                sum_x = 0.0;
-                sum_y = 0.0;
-                for ( let i = 0; i < this.relative_path_points_ccw.length; i++ ) {
-                    const curr_x = this.relative_path_points_ccw[i][0]; // + com[0];
-                    const curr_y = this.relative_path_points_ccw[i][1]; // + com[1];
-                    sum_x += curr_x;
-                    sum_y += curr_y;
-                }
-                sum_x *= recip_num_pts;
-                sum_y *= recip_num_pts;
-                console.log( ` *** com_v test, should be (0|0) = ${new Vec2(sum_x, sum_y)}` );        
-            }
+            // {
+            //     sum_x = 0.0;
+            //     sum_y = 0.0;
+            //     for ( let i = 0; i < this.relative_path_points_ccw.length; i++ ) {
+            //         const curr_x = this.relative_path_points_ccw[i][0]; // + com[0];
+            //         const curr_y = this.relative_path_points_ccw[i][1]; // + com[1];
+            //         sum_x += curr_x;
+            //         sum_y += curr_y;
+            //     }
+            //     sum_x *= recip_num_pts;
+            //     sum_y *= recip_num_pts;
+            //     console.log( ` *** com_v test, should be (0|0) = ${new Vec2(sum_x, sum_y)}` );        
+            // }
             
             // Center of Mass is good, BUT for the bounding radius, we actually need the smallest circle enclosing all vertices
             
             const bounding_circle = BoundingCircle.makeCircle( this.relative_path_points_ccw );
             
-            for ( let i = 0; i < this.relative_path_points_ccw.length; i++ ) {
-                this.relative_path_points_ccw[i][0] -= bounding_circle[0];
-                this.relative_path_points_ccw[i][1] -= bounding_circle[1];
-            }           
+            // for ( let i = 0; i < this.relative_path_points_ccw.length; i++ ) {
+            //     this.relative_path_points_ccw[i][0] -= bounding_circle[0];
+            //     this.relative_path_points_ccw[i][1] -= bounding_circle[1];
+            // }           
             this.bound_radius = bounding_circle[2];
-            
-            // // const com2_v = new Vec2( com[0], com[1] );
-            // // console.log( `com_v = ${com_v} vs. com2_v = ${com2_v}` );
-            // let max_r = 0.0;
-            // this.relative_path_points_ccw.forEach( (path_pt) => {
-            //     const curr_v = Vec2.fromArray( path_pt );
-            //     console.log( `  # curr_v = ${curr_v}, len = ${curr_v.len()}` );
-            //     //max_r = Math.max( max_r, Vec2.dist( curr_v, com_v ) );
-            //     max_r = Math.max( max_r, curr_v.len() );
-            // } );
-            // this.bound_radius = max_r;
-
+            this.bounding_circle = [ [ bounding_circle[0], bounding_circle[1] ], bounding_circle[2] ];
+        }
+        
+        return this.bounding_circle;
+    }
+    
+    getBoundRadius() {
+        if ( this.bound_radius < 0.0 ) {
+                
+            this.getBoundingCircle();
             console.log( `this.bound_radius = ${this.bound_radius}` );
         }
         return this.bound_radius;
