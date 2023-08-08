625d3d87b08d004dcb9c48f31c2dc70f215a59f7
P H <p.houska@cgv.tugraz.at>
2023-08-08 17:28:22 +0200

    putting together game objects (rigid-bodies + new circle shape) for testing in the app

--- js/drawPrimitive.js
+++ js/drawPrimitive.js
@@ -17,9 +17,103 @@ var MyDrawPrimitive = (function (exports) {
         return [ pivot_x, pivot_y ];
     }
     
-    let setupTriangle = function setupTriangleFn( ccw_p0, ccw_p1, ccw_p2 ) {
+    let defaultVertexSource = function defaultVertexSourceFn() {
+        const vertexSrc = `
+            precision mediump float;
+            attribute vec2 aVertexPosition;
+            attribute vec2 aUvs;
+            uniform mat3 translationMatrix;
+            uniform mat3 projectionMatrix;
+            uniform mat3 u_viewMatrix;
+            varying vec2 vUvs;
+            void main() {
+                vUvs = aUvs;
+                gl_Position = vec4((projectionMatrix * u_viewMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+            }`;
+        return vertexSrc;
+    }
+    
+    let defaultFragmentSource = function defaultFragmentSourceFn() {
+        const fragmentSrc = `
+            precision mediump float;
+            varying vec2 vUvs;
+            uniform sampler2D u_Sampler2;
+            uniform float u_time;
+            uniform vec4 u_color;
+            void main() {
+                gl_FragColor = u_color;
+                //gl_FragColor = u_color * texture2D(u_Sampler2, vUvs + sin( (u_time + (vUvs.x) * 14.) ) * 0.1 );
+            }`;
+        return fragmentSrc;        
+    }
 
-        const vertex_positions = [  
+    let setupCircle = function setupCircleFn( center_point, radius, num_segments ) {
+        
+        let vertex_positions = [];
+        let tex_coords = [];
+        if ( num_segments == undefined || num_segments <= 0 ) { num_segments = 20.0; }
+        
+        console.log( `num_segments = ${num_segments}` );
+        
+        const segment_step = 2.0 * Math.PI / num_segments;
+        for ( let i = 0; i < 2.0 * Math.PI; i += segment_step ) {
+            const cos_i = Math.cos( i );
+            const sin_i = Math.sin( i );
+            vertex_positions.push( center_point[0] + cos_i * radius );
+            vertex_positions.push( center_point[1] + sin_i * radius );
+            // vertex_positions.push( cos_i * radius );
+            // vertex_positions.push( sin_i * radius );
+            tex_coords.push( cos_i * 0.5 + 0.5 );
+            tex_coords.push( sin_i * 0.5 + 0.5 );
+        }
+        
+        // TODO: just return setupPolygon() with the calculated vertex positions !!!
+        return this.setupPolygon( vertex_positions, tex_coords );
+        
+        // const indices = [...Array(vertex_positions.length/2).keys()];
+
+        // const geometry = new PIXI.Geometry()
+        // .addAttribute('aVertexPosition', // the attribute name
+        //     vertex_positions, // x, y
+        //     2) // the size of the attribute
+        // .addAttribute('aUvs', // the attribute name
+        //     tex_coords, // u, v
+        //     2) // the size of the attribute
+        // .addIndex(indices);
+    
+        // const vertexSrc = defaultVertexSource();
+        // const fragmentSrc = defaultFragmentSource();
+        
+        // viewMat3 = new PIXI.Matrix();
+
+        // const uniforms = {
+        //     u_viewMatrix: viewMat3,
+        //     //uSampler2: PIXI.Texture.from('examples/assets/bg_scene_rotate.jpg'),
+        //     u_Sampler2: PIXI.Texture.from('assets/flowerTop.png'),
+        //     u_color: [0.5,0.0,0.0,0.5],
+        //     u_time: 0,
+        // };
+        
+        // const shader = PIXI.Shader.from(vertexSrc, fragmentSrc, uniforms);
+        
+        // const circle = new PIXI.Mesh(geometry, shader);
+        
+        // //const [ pivot_x, pivot_y ] = calcPivotForPrimitive( vertex_positions );
+        // //const [ pivot_x, pivot_y ] = [ center_point[0], center_point[1] ];
+        // const pivot_x = center_point[0];
+        // const pivot_y = center_point[1];
+        // circle.pivot.set( pivot_x, pivot_y );
+        // circle.position.set( pivot_x, pivot_y );
+        
+        // circle.drawMode = PIXI.DRAW_MODES.TRIANGLE_FAN;
+        
+        // return circle;
+    }
+    
+    
+    let setupTriangle = function setupTriangleFn( ccw_p0, ccw_p1, ccw_p2 ) {
+        
+        let vertex_positions = [  
             // -100, -100, // x, y
             // 100, -100, // x, y
             // 0, 100 // x, y
@@ -27,6 +121,11 @@ var MyDrawPrimitive = (function (exports) {
             ccw_p1[0], ccw_p1[1], // x, y
             ccw_p2[0], ccw_p2[1], // x, y
         ];
+        const [ pivot_x, pivot_y ] = calcPivotForPrimitive( vertex_positions );
+        // for ( let i = 0; i < 6; i += 2 ) {
+        //     vertex_positions[ i + 0 ] -= pivot_x;
+        //     vertex_positions[ i + 1 ] -= pivot_y;
+        // }
 
         const geometry = new PIXI.Geometry()
         .addAttribute('aVertexPosition', // the attribute name
@@ -39,42 +138,31 @@ var MyDrawPrimitive = (function (exports) {
             2) // the size of the attribute
         .addIndex([0, 1, 2]);
     
-        const vertexSrc = `
+        // const vertexSrc = `
+        //     precision mediump float;
+        //     attribute vec2 aVertexPosition;
+        //     attribute vec2 aUvs;
+        //     uniform mat3 translationMatrix;
+        //     uniform mat3 projectionMatrix;
+        //     uniform mat3 u_viewMatrix;
+        //     varying vec2 vUvs;
+        //     void main() {
+        //         vUvs = aUvs;
+        //         gl_Position = vec4((projectionMatrix * u_viewMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+        //     }`;
         
-            precision mediump float;
-        
-            attribute vec2 aVertexPosition;
-            attribute vec2 aUvs;
-        
-            uniform mat3 translationMatrix;
-            uniform mat3 projectionMatrix;
-            
-            uniform mat3 u_viewMatrix;
-            
-            varying vec2 vUvs;
-        
-            void main() {
-        
-                vUvs = aUvs;
-                gl_Position = vec4((projectionMatrix * u_viewMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
-        
-            }`;
-        
-        const fragmentSrc = `
-        
-            precision mediump float;
-        
-            varying vec2 vUvs;
-        
-            uniform sampler2D u_Sampler2;
-            uniform float u_time;
-            uniform vec4 u_color;
-        
-            void main() {
-        
-                gl_FragColor = u_color;
-                //gl_FragColor = u_color * texture2D(u_Sampler2, vUvs + sin( (u_time + (vUvs.x) * 14.) ) * 0.1 );
-            }`;
+        // const fragmentSrc = `
+        //     precision mediump float;
+        //     varying vec2 vUvs;
+        //     uniform sampler2D u_Sampler2;
+        //     uniform float u_time;
+        //     uniform vec4 u_color;
+        //     void main() {
+        //         gl_FragColor = u_color;
+        //         //gl_FragColor = u_color * texture2D(u_Sampler2, vUvs + sin( (u_time + (vUvs.x) * 14.) ) * 0.1 );
+        //     }`;
+        const vertexSrc = defaultVertexSource();
+        const fragmentSrc = defaultFragmentSource();
         
         viewMat3 = new PIXI.Matrix();
         //viewMat3.rotate( Math.PI * 0.33 );
@@ -93,7 +181,6 @@ var MyDrawPrimitive = (function (exports) {
         
         const triangle = new PIXI.Mesh(geometry, shader);
         
-        const [ pivot_x, pivot_y ] = calcPivotForPrimitive( vertex_positions );
         triangle.pivot.set( pivot_x, pivot_y );
         triangle.position.set( pivot_x, pivot_y );
         
@@ -126,38 +213,29 @@ var MyDrawPrimitive = (function (exports) {
             2) // the size of the attribute
         .addIndex([0, 1, 2, 0, 2, 3]);
     
-        const vertexSrc = `
+        // const vertexSrc = `
+        //     precision mediump float;
+        //     attribute vec2 aVertexPosition;
+        //     attribute vec2 aUvs;
+        //     uniform mat3 translationMatrix;
+        //     uniform mat3 projectionMatrix;
+        //     varying vec2 vUvs;
+        //     void main() {
+        //         vUvs = aUvs;
+        //         gl_Position = vec4((projectionMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+        //     }`;
         
-            precision mediump float;
-        
-            attribute vec2 aVertexPosition;
-            attribute vec2 aUvs;
-        
-            uniform mat3 translationMatrix;
-            uniform mat3 projectionMatrix;
-        
-            varying vec2 vUvs;
-        
-            void main() {
-        
-                vUvs = aUvs;
-                gl_Position = vec4((projectionMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
-        
-            }`;
-        
-        const fragmentSrc = `
-        
-            precision mediump float;
-        
-            varying vec2 vUvs;
-        
-            uniform sampler2D uSampler2;
-            uniform float time;
-        
-            void main() {
-        
-                gl_FragColor = texture2D(uSampler2, vUvs + sin( (time + (vUvs.x) * 14.) ) * 0.1 );
-            }`;
+        // const fragmentSrc = `
+        //     precision mediump float;
+        //     varying vec2 vUvs;
+        //     uniform sampler2D uSampler2;
+        //     uniform float time;
+        //     void main() {
+        //         gl_FragColor = texture2D(uSampler2, vUvs + sin( (time + (vUvs.x) * 14.) ) * 0.1 );
+        //     }`;
+
+        const vertexSrc = defaultVertexSource();
+        const fragmentSrc = defaultFragmentSource();
         
         const uniforms = {
             //uSampler2: PIXI.Texture.from('examples/assets/bg_scene_rotate.jpg'),
@@ -181,7 +259,13 @@ var MyDrawPrimitive = (function (exports) {
 
     
     
-    let setupPolygon = function setupPolygonFn( vertex_positions, texture_coords, indices ) {
+    let setupPolygon = function setupPolygonFn( vertex_positions, texture_coords/*, indices*/ ) {
+        const indices = [...Array(vertex_positions.length/2).keys()];
+        
+        if ( texture_coords == undefined || texture_coords.length == 0 ) {
+            texture_coords = vertex_positions;
+        }
+        
         const geometry = new PIXI.Geometry()
         .addAttribute('aVertexPosition', // the attribute name
             vertex_positions, // x, y
@@ -191,42 +275,32 @@ var MyDrawPrimitive = (function (exports) {
             2 ) // the size of the attribute (2D texture coordinates)
         .addIndex(indices);
     
-        const vertexSrc = `
+        // const vertexSrc = `
+        //     precision mediump float;
+        //     attribute vec2 aVertexPosition;
+        //     attribute vec2 aUvs;
+        //     uniform mat3 translationMatrix;
+        //     uniform mat3 projectionMatrix;
+        //     uniform mat3 u_viewMatrix;
+        //     varying vec2 vUvs;
+        //     void main() {
+        //         vUvs = aUvs;
+        //         gl_Position = vec4((projectionMatrix * u_viewMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+        //     }`;
         
-            precision mediump float;
-        
-            attribute vec2 aVertexPosition;
-            attribute vec2 aUvs;
-        
-            uniform mat3 translationMatrix;
-            uniform mat3 projectionMatrix;
-            
-            uniform mat3 u_viewMatrix;
-            
-            varying vec2 vUvs;
-        
-            void main() {
-        
-                vUvs = aUvs;
-                gl_Position = vec4((projectionMatrix * u_viewMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
-        
-            }`;
-        
-        const fragmentSrc = `
-        
-            precision mediump float;
-        
-            varying vec2 vUvs;
-        
-            uniform sampler2D u_Sampler2;
-            uniform float u_time;
-            uniform vec4 u_color;
-        
-            void main() {
-        
-                gl_FragColor = u_color;
-                //gl_FragColor = u_color * texture2D(u_Sampler2, vUvs + sin( (u_time + (vUvs.x) * 14.) ) * 0.1 );
-            }`;
+        // const fragmentSrc = `
+        //     precision mediump float;
+        //     varying vec2 vUvs;
+        //     uniform sampler2D u_Sampler2;
+        //     uniform float u_time;
+        //     uniform vec4 u_color;
+        //     void main() {
+        //         gl_FragColor = u_color;
+        //         //gl_FragColor = u_color * texture2D(u_Sampler2, vUvs + sin( (u_time + (vUvs.x) * 14.) ) * 0.1 );
+        //     }`;
+
+        const vertexSrc = defaultVertexSource();
+        const fragmentSrc = defaultFragmentSource();
         
         viewMat3 = new PIXI.Matrix();
         //viewMat3.rotate( Math.PI * 0.33 );
@@ -249,11 +323,15 @@ var MyDrawPrimitive = (function (exports) {
         polygon.pivot.set( pivot_x, pivot_y );
         polygon.position.set( pivot_x, pivot_y );
 
+        //polygon.drawMode = PIXI.DRAW_MODES.TRIANGLE_STRIP;
+        polygon.drawMode = PIXI.DRAW_MODES.TRIANGLE_FAN;
+        
         return polygon;
     }
     
 
     exports.calcPivotForPrimitive = calcPivotForPrimitive;
+    exports.setupCircle = setupCircle;
     exports.setupTriangle = setupTriangle;
     exports.setupQuad = setupQuad;
     exports.setupPolygon = setupPolygon;
