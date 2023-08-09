b13583cffcf3ab1896ff1c16c9ed24291b05c6f5
P H <p.houska@cgv.tugraz.at>
2023-08-09 22:43:03 +0200

    added utility function that converts normalized float rgbs from [0,1] to hex 24 bit

--- js/mathUtil.js
+++ js/mathUtil.js
@@ -6,4 +6,8 @@ class MathUtil {
         return ( Math.abs( x - y ) < zero_tolerance );
     }
     
+    //static normalizedFloatToByte( 
+    static rgbFloatsToHexColor( float_array ) {
+        return ( ( ( ( float_array[0] * 255.0 + float_array[1] ) * 255.0 + float_array[2] ) * 255.0 ) & 0xFFFFFF );
+    }
 }
