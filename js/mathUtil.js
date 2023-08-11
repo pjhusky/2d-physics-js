1298d3a4dac4946d06e4e629718d3718471da934
P H <p.houska@cgv.tugraz.at>
2023-08-12 01:04:49 +0200

    had forgotten to commit change for param t * be used at caller to determine pt along edge with closest dist to pt * is used in collision.js

--- js/mathUtil.js
+++ js/mathUtil.js
@@ -1,9 +1,9 @@
 class MathUtil {
 
     // adapted from https://stackoverflow.com/questions/23615299/how-to-specify-float32-limits-in-javascript
-    static f32_LargestPosVal() { return 3.40282347e+38; } // largest positive number in float32
+    static f32_LargestPosVal() { return  3.40282347e+38; } // largest positive number in float32
     static f32_LargestNegVal() { return -3.40282347e+38; } // largest negative number in float32
-    static f32_Eps() { return 1.175494351e-38; } // smallest number in float32
+    static f32_Eps()           { return  1.175494351e-38; } // smallest number in float32
 
     static isApproxEqual( x, y ) {
         // const zero_tolerance = 0.0000001;
@@ -21,10 +21,10 @@ class MathUtil {
     }
     
     // adapted from https://stackoverflole_vec2.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment
-    static distPointToLineSegment( p_vec2, ls_vec2, le_vec2 ) {
+    static distPointToLineSegment( p_vec2, ls_vec2, le_vec2, t ) {
         var l2 = Vec2.distSquared(ls_vec2, le_vec2);
         if ( this.isApproxEqual( l2, 0.0 ) ) { return Vec2.distSquared(p_vec2, ls_vec2); }
-        var t = ((p_vec2.x - ls_vec2.x) * (le_vec2.x - ls_vec2.x) + (p_vec2.y - ls_vec2.y) * (le_vec2.y - ls_vec2.y)) / l2;
+        t = ((p_vec2.x - ls_vec2.x) * (le_vec2.x - ls_vec2.x) + (p_vec2.y - ls_vec2.y) * (le_vec2.y - ls_vec2.y)) / l2;
         t = Math.max(0, Math.min(1, t));
         return Vec2.dist( p_vec2, new Vec2( ls_vec2.x + t * (le_vec2.x - ls_vec2.x),
                                             ls_vec2.y + t * (le_vec2.y - ls_vec2.y) ) );
