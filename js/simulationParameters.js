e0d3b1b3c8ff584810bb66f0643b44f0e031131b
P H <p.houska@cgv.tugraz.at>
2023-08-18 02:32:50 +0200

    misc: tweaked simulation parameters (more gravity)

--- js/simulationParameters.js
+++ js/simulationParameters.js
@@ -5,9 +5,9 @@ import { Vec2 } from './vec2.js';
 export
 class SimulationParameters {
     
-    static globalGravity() { return new Vec2( 0.0, 9.81 * 5.0 ); }
+    static globalGravity() { return new Vec2( 0.0, 9.81 * 12.0 ); }
     static globalLinearFriction() { return ( 1.0 - 0.9875 ); }
-    static globalAngularFriction() { return ( 1.0 - 0.98 ); }
+    static globalAngularFriction() { return ( 1.0 - 0.975 ); }
     static globalMaxPenetrationRelaxationIterations() { return 20.0; }
     static globalPerformPenetrationRelaxation() { return true; }
 }
